0e3470c706c5aa25ba5e86a12d05cba858a70e78
Pedro Goiania <pedro.paulods@hotmail.com>
2026-02-18 13:08:46 -0300

    created mutations for locations

--- src/dataSources/manager-integration/index.js
+++ src/dataSources/manager-integration/index.js
@@ -2,12 +2,14 @@ import speakers from './speakers';
 import agendas from './agendas';
 import comments from './comments';
 import events from './events';
+import locations from './locations';
 
 const managerIntegration = ({ headers }) => ({
   ...speakers({ headers }),
   ...agendas({ headers }),
   ...comments({ headers }),
   ...events({ headers }),
+  ...locations({ headers }),
 });
 
 export default managerIntegration;
